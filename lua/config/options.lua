74af0cd32b4adad6b9ed13fd8455a54455d8ba3b
Phat Intern <coolcore.xix@gmail.com>
2026-01-03 21:50:53 +0700

    Update lazy-lock.json to include bufferline.nvim and set leader keys in options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Add any additional options here
+
+-- Set leader key to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
+    opts = {
+      options = {
+        mode = "buffers",
+        themable = true,
+        numbers = "ordinal", -- "none" | "ordinal" | "buffer_id" | "both"
+        close_command = "bdelete! %d",
+        right_mouse_command = "bdelete! %d",
+        left_mouse_command = "buffer %d",
+        middle_mouse_command = nil,
+        indicator = {
+          icon = "▎",
+          style = "icon",
+        },
+        buffer_close_icon = "󰅖",
+        modified_icon = "●",
+        close_icon = "󰅖",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
+        max_name_length = 18,
+        max_prefix_length = 15,
+        tab_size = 18,
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = false,
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          },
+        },
+        color_icons = true,
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+        show_duplicate_prefix = true,
+        persist_buffer_sort = true,
+        move_wraps_at_ends = false,
+        separator_style = "thin", -- "slant" | "slope" | "thick" | "thin" | { "any", "any" },
+        enforce_regular_tabs = false,
+        always_show_bufferline = true,
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { "close" },
+        },
+        sort_by = "insert_after_current",
+      },
+    },
+  },
+}
+
